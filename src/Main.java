57c61e8a3785350f5c8c0e7ba3b48e3a40a4e66f
Rashika1998 <rashikamr1998@gmail.com>
2022-01-10 16:59:52 +0530

    filter and sort functions.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,52 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args){
 
         List<Person> people = getPeople();
-
         List<Person> females = new ArrayList<>();
+
+        // Imperative approach
 
         for (Person person : people){
             if(person.getGender().equals(Gender.FEMALE)){
                 females.add(person);
             }
         }
+        //females.forEach(System.out::println);
 
-        females.forEach(System.out::println);
 
+
+
+        //Declarative approach
+
+        //Filter functionality
+        List<Person> filterFemales = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .collect(Collectors.toList());
+
+        filterFemales.forEach(System.out::println);
+        System.out.println("\n");
+        System.out.println("\n");
+
+        //Sort functionality
+        List<Person> sortedPeople = people.stream()
+                .sorted(Comparator.comparing(Person::getAge).reversed())
+                        .collect(Collectors.toList());
+
+        sortedPeople.forEach(System.out::println);
+        System.out.println("\n");
+        System.out.println("\n");
 
     }
 
     private static List<Person> getPeople(){
-        return List.of(
+        return List.of
+        (
                 new Person("Antonio", 20, Gender.MALE),
                 new Person("Alina Smith", 33, Gender.FEMALE),
                 new Person("Helen White", 57, Gender.FEMALE),
